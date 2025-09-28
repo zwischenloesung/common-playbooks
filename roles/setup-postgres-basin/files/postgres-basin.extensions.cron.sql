7245e367226d4b5e4bf17379f8e03df8aafbec64
Michael Lustenberger <mic@inofix.ch>
2025-09-29 00:32:50 +0200

    PostgreSQL-Basin: Cron Clean Up
    Make pg_cron optional, but assume >=1.3.

---
+++ roles/setup-postgres-basin/files/postgres-basin.extensions.cron.sql
@@ -0,0 +1,5 @@
+-- # add missing extensions
+
+-- pg_cron for periodic tasks of course
+CREATE EXTENSION IF NOT EXISTS pg_cron;
+
--- roles/setup-postgres-basin/files/postgres-basin.extensions.sql
+++ roles/setup-postgres-basin/files/postgres-basin.extensions.sql
@@ -3,6 +3,3 @@
 -- ltree to uncover device tree
 CREATE EXTENSION IF NOT EXISTS ltree;
 
--- pg_cron for periodic tasks of course
-CREATE EXTENSION IF NOT EXISTS pg_cron;
-
